430b4d1664800df157a7088a994f409e68d8ca96
RuthBiney <r.biney@alutstudent.com>
2024-01-17 17:50:56 +0200

    Howdy Partner

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-cat_arrays = __import__('6-howdy_partner').cat_arrays
+"cat_arrays = __import__('6-howdy_partner').cat_arrays"
 def cat_arrays(arr1, arr2):
     # Use the + operator to concatenate the two arrays
     result = arr1 + arr2
