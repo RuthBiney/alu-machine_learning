3085ad3f9a3d008110245abc69ee7e7fbcde5047
RuthBiney <r.biney@alutstudent.com>
2024-01-17 17:44:46 +0200

    changes to Line Up

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-add_arrays = __import__('4-line_up').add_arrays
+"add_arrays = __import__('4-line_up')"
 def add_arrays(arr1, arr2):
     """Add two arrays element-wise."""
     if len(arr1) != len(arr2):
