e0af55b38ff5e25d63cef2054b12a70bc7d63163
RuthBiney <r.biney@alutstudent.com>
2024-01-18 21:25:42 +0200

    Pycodestyle validation

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
 "matrix_transpose = __import__('3-flip_me_over').matrix_transpose"
+
+
 def matrix_transpose(matrix):
     """Return the transpose of a 2D matrix."""
     # Transpose the matrix using list comprehension
-    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
-    return result
+    return [
+        [
+            matrix[j][i] 
+            for j in range(len(matrix))] 
+            for i in range(len(matrix[0]))
+        ]
