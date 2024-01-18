1fd3bfd8228a30700f08f5e7acda825ca6061890
RuthBiney <r.biney@alutstudent.com>
2024-01-18 21:40:41 +0200

    Pycodestyle validation and everything is documented changes

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 "cat_arrays = __import__('6-howdy_partner').cat_arrays"
+
+
 def cat_arrays(arr1, arr2):
-    # Use the + operator to concatenate the two arrays
+    """Use the + operator to concatenate the two arrays"""
     result = arr1 + arr2
     return result
