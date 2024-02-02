ae13087a6b8f8b4c8a27919e90846a7a9eff6519
RuthBiney <r.biney@alutstudent.com>
2024-02-02 14:55:20 +0200

    fixed

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
-# '''
-#     a function def poly_derivative(poly):
-#     that calculates the derivative of a polynomial
+'''
+    a function def poly_derivative(poly):
+    that calculates the derivative of a polynomial
 
-#     Parameters:
-#     poly (list): A list of coefficients.
-# '''
+    Parameters:
+    poly (list): A list of coefficients.
+'''
 
 
 def poly_derivative(poly):
     # Check if poly is not a list or is an empty list
     if not isinstance(poly, list) or not poly:
         return None
-    # Check if the polynomial represents a constant (degree 0)
     if len(poly) == 1:
         return [0]
-    # Calculate the derivative
+
     return [poly[i] * i for i in range(1, len(poly))]
