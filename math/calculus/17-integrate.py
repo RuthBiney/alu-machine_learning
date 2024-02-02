40591597ff2e9aeeba6e843f6b555013730d2af8
RuthBiney <r.biney@alutstudent.com>
2024-02-02 14:31:03 +0200

    Integrate

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,33 +1,30 @@
 #!/usr/bin/env python3
+'''
+    defines a function that calculates
+    the integral of a polynomial
+'''
+
+
 def poly_integral(poly, C=0):
-    # Check if the input is valid
-    if not isinstance(poly, list) or not all(isinstance(coeff, (int, float)) for coeff in poly) or not isinstance(C, (int, float)):
+    """
+    calculates the integral of the given polynomial
+    """
+    if type(poly) is not list or len(poly) < 1:
         return None
-
-    # Check if the polynomial is not empty
-    if len(poly) == 0:
+    if type(C) is not int and type(C) is not float:
         return None
-
-    # Calculate the integral
-    integral_poly = [C]  # Start with the integration constant
-    for power, coeff in enumerate(poly):
-        if coeff == 0:
-            integral_poly.append(0)
+    for coefficient in poly:
+        if type(coefficient) is not int and type(coefficient) is not float:
+            return None
+    if type(C) is float and C.is_integer():
+        C = int(C)
+    integral = [C]
+    for power, coefficient in enumerate(poly):
+        if (coefficient % (power + 1)) is 0:
+            new_coefficient = coefficient // (power + 1)
         else:
-            # The integral of x^n is x^(n+1)/(n+1)
-            integral_coeff = coeff / (power + 1)
-            # If the integral coefficient is a whole number, convert it to an integer
-            integral_coeff = int(
-                integral_coeff) if integral_coeff.is_integer() else integral_coeff
-            integral_poly.append(integral_coeff)
-
-    # Remove trailing zeroes to make the list as small as possible
-    while len(integral_poly) > 1 and integral_poly[-1] == 0:
-        integral_poly.pop()
-
-    return integral_poly
-
-
-# Example usage:
-poly = [5, 3, 0, 1]
-print(poly_integral(poly))  # This should print [0, 5, 1.5, 0, 0.25]
+            new_coefficient = coefficient / (power + 1)
+        integral.append(new_coefficient)
+    while integral[-1] is 0 and len(integral) > 1:
+        integral = integral[:-1]
+    return integral
