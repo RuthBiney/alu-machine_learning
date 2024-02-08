b153ffcb367e10051afeb9f50ce7f522b588e521
RuthBiney <r.biney@alutstudent.com>
2024-02-08 14:36:28 +0200

    Poisson PMF

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -3,12 +3,18 @@ from math import exp, factorial
 
 
 class Poisson:
+    """
+    Represents a Poisson distribution.
 
-    "Represents a Poisson distribution."
+    Attributes:
+        lambtha (float): The expected number of occurrences in a given time frame.
+
+    Methods:
+        __init__(self, data=None, lambtha=1.): Initializes a Poisson distribution.
+        pmf(self, k): Calculates the PMF (Probability Mass Function) for a given number of successes.
+    """
 
     def __init__(self, data=None, lambtha=1.):
-        "Initializes the Poisson distribution either by estimating lambtha"
-
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
@@ -21,11 +27,18 @@ class Poisson:
             self.lambtha = float(sum(data) / len(data))
 
     def pmf(self, k):
-        "Calculates the Probability Mass Function (PMF)"
+        """
+        Calculates the Probability Mass Function (PMF) for a given number of successes.
 
-        k = int(k)  # Ensure k is an integer
+        Parameters:
+            k (int or float): The number of successes for which to calculate the PMF.
+
+        Returns:
+            float: The PMF value for k successes, or 0 if k is out of range.
+        """
+        k = int(k)  # Convert k to an integer
         if k < 0:
             return 0  # Return 0 if k is out of range (negative)
-        else:
-            # Calculate and return the PMF value
-            return (self.lambtha ** k) * exp(-self.lambtha) / factorial(k)
+
+        # Calculate and return the PMF value
+        return (self.lambtha ** k) * exp(-self.lambtha) / factorial(k)
