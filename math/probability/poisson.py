507dafcdd6d54db95340e345c9a19e57bb1abff4
RuthBiney <r.biney@alutstudent.com>
2024-02-08 15:04:54 +0200

    Changes

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
-import numpy as np  # Import numpy module
-
-"""Calculate poisson PMF"""
-
-
+# Define the Poisson class
 class Poisson:
     def __init__(self, data=None, lambtha=1.):
-        """Calculate poisson PMF"""
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
@@ -16,7 +11,7 @@ class Poisson:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = float(sum(data) / len(data))
+            self.lambtha = sum(data) / len(data)
 
     def factorial(self, k):
         """Calculates the factorial of k."""
@@ -43,8 +38,7 @@ class Poisson:
 
 
 # Using the class
-np.random.seed(0)
-data = np.random.poisson(5., 100).tolist()
+data = [5] * 100  # Creating a list of 100 elements with value 5
 p1 = Poisson(data)
 print('P(9):', round(p1.pmf(9), 10))  # Rounding to 10 decimal places
 
