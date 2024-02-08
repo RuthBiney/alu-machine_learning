2f29c46b186e706a31e85163b73da7854281c601
RuthBiney <r.biney@alutstudent.com>
2024-02-08 14:14:48 +0200

    Initialize Poisson

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,30 +1,14 @@
 #!/usr/bin/env python3
+"The Poisson class supports initializing a distribution"
+
+
 class Poisson:
-    """
-    Represents a Poisson distribution.
 
-    Attributes:
-        lambtha (float): The expected number of occurrences in a given time frame.
-
-    Methods:
-        __init__(self, data=None, lambtha=1.): Initializes a Poisson distribution.
-    """
+    "Represents a Poisson distribution."
 
     def __init__(self, data=None, lambtha=1.):
-        """
-        Initializes the Poisson distribution either by estimating lambtha from data
-        or using a provided lambtha value.
+        "Initializes the Poisson distribution"
 
-        Parameters:
-            data (list, optional): A list of data points to estimate lambtha. Default is None.
-            lambtha (float, optional): The expected number of occurrences in a given time frame.
-                                       This is used if data is None. Default is 1.
-
-        Raises:
-            ValueError: If lambtha is not a positive value when data is None.
-            TypeError: If data is provided but is not a list.
-            ValueError: If data is provided but does not contain multiple values.
-        """
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
