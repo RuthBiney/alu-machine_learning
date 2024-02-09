85573b22f4fee77c8291a6a4477b81bb82829930
RuthBiney <r.biney@alutstudent.com>
2024-02-09 13:47:29 +0200

    Poisson PMF

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,63 +1,41 @@
 #!/usr/bin/env python3
-'''
-    Poisson distribution
-    that represents a poisson distribution
-'''
+"""Poisson Distribution"""
 
 
 class Poisson:
-    '''
-        Class Poisson that represents a
-        distribution of Poisson
-    '''
-
-    def factorial(self, k):
-        '''
-            Calculates the factorial
-        '''
-        if k < 0:
-            return 0
-        if k == 0 or k == 1:
-            return 1
-        return k * self.factorial(k - 1)
-
     def __init__(self, data=None, lambtha=1.):
-        '''
-            Class constructor
-        '''
+        """ Class Poisson that represents a distribution of Poisson"""
         if data is None:
             if lambtha <= 0:
-                raise ValueError('lambtha must be a positive value')
+                raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
         else:
-            if type(data) is not list:
-                raise TypeError('data must be a list')
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
             if len(data) < 2:
-                raise ValueError('data must contain multiple values')
+                raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
+            if self.lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
 
     def pmf(self, k):
-        '''
-            Calculates the value of the
-            PMF for a given number of successes
-        '''
-        if k < 0:
-            return 0
+        """Calculate pmf"""
         k = int(k)
-        e = 2.7182818285
-        return ((self.lambtha ** k) * (e ** (-self.lambtha))
-                ) / (self.factorial(k))
-
-    def cdf(self, k):
-        '''
-            Calculates the value of the
-            CDF for a given number of successes
-        '''
-        if type(k) is not int:
-            k = int(k)
         if k < 0:
             return 0
-        cdf = 0
-        for i in range(k + 1):
-            cdf += self.pmf(i)
-        return cdf
+        else:
+            return (self.lambtha ** k) * (2.7182818285 ** (-self.lambtha)) / self.factorial(k)
+
+    def factorial(self, n):
+        """Calculate factorial"""
+        if n == 0:
+            return 1
+        else:
+            return n * self.factorial(n - 1)
+
+
+# Test cases
+if __name__ == "__main__":
+    p = Poisson(data=[1, 2, 3])
+    print("{:.10f}".format(p.pmf(2)))  # Should print the PMF value for k = 2
+    print("{:.10f}".format(p.pmf(1.5)))  # Should print the PMF value for k = 1
