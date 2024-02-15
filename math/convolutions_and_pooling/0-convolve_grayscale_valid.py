8d81622173ddc430496081859a31ce177189ad13
RuthBiney <r.biney@alutstudent.com>
2024-02-15 16:41:05 +0200

    pycodestyle

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" a function def convolve_grayscale_valid(images, kernel)"""
+
 import numpy as np
 
 
@@ -8,11 +8,13 @@ def convolve_grayscale_valid(images, kernel):
     Perform a valid convolution on grayscale images.
 
     Args:
-    - images: a numpy.ndarray with shape (m, h, w) containing multiple grayscale images
+    - images: a numpy.ndarray with shape (m, h, w) 
+    containing multiple grayscale images
               m is the number of images
               h is the height in pixels of the images
               w is the width in pixels of the images
-    - kernel: a numpy.ndarray with shape (kh, kw) containing the kernel for the convolution
+    - kernel: a numpy.ndarray with shape (kh, kw) 
+    containing the kernel for the convolution
               kh is the height of the kernel
               kw is the width of the kernel
 
@@ -28,7 +30,7 @@ def convolve_grayscale_valid(images, kernel):
     output_w = w - kw + 1
 
     # Initialize the output array
-    output = np.zeros((m, output_h, output_w))
+    convolved_images = np.zeros((m, output_h, output_w))
 
     # Flip the kernel
     kernel = np.flipud(np.fliplr(kernel))
@@ -40,6 +42,7 @@ def convolve_grayscale_valid(images, kernel):
             # Iterate over width
             for k in range(output_w):
                 # Compute the convolution for the current position
-                output[i, j, k] = np.sum(images[i, j:j+kh, k:k+kw] * kernel)
+                convolved_images[i, j, k] = np.sum(
+                    images[i, j:j+kh, k:k+kw] * kernel)
 
-    return output
+    return convolved_images
