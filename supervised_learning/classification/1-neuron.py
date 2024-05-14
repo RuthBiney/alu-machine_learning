8509de33d0e9c3ee814beb63351fbeb7148dccb7
RuthBiney <r.biney@alustudent.com>
2024-05-14 22:00:41 +0200

    Privatize Neuron

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+'''
+    A class Neuron that defines a single neuron performing
+    binary classification:
+'''
+
+
+import numpy as np
+
+
+class Neuron:
+    '''
+        Class Neuron
+    '''
+    def __init__(self, nx):
+        """
+        Constructor for Neuron class.
+
+        Args:
+        nx (int): Number of input features to the neuron.
+
+        Raises:
+        TypeError: If nx is not an integer.
+        ValueError: If nx is less than 1.
+        """
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        '''
+            Getter
+        '''
+        return self.__W
+
+    @property
+    def b(self):
+        '''
+            Getter
+        '''
+        return self.__b
+
+    @property
+    def A(self):
+        '''
+            Getter
+        '''
+        return self.__A
+    
