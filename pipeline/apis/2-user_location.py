2641145d95ed02c7c111bf54ec9354909893a506
RuthBiney <r.biney@alustudent.com>
2024-06-29 17:11:28 +0200

    Rate me is you can!

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,48 +1,31 @@
 #!/usr/bin/env python3
 '''
-Prints the location of a user
+    This module returns the location of a user
+    with a github api url.
 '''
-
-
-import sys
 import requests
+import sys
 import time
 
 
 def get_user_location(api_url):
-    """
-    Fetch and print the location of a GitHub user.
-
-    :param api_url: The API URL for the user
-    """
-    try:
-        response = requests.get(api_url)
-
-        if response.status_code == 200:
-            user_data = response.json()
-            location = user_data.get('location')
-            if location:
-                print(location)
-            else:
-                print('Location not available')
-        elif response.status_code == 404:
-            print('Not found')
-        elif response.status_code == 403:
-            reset_time = int(
-                response.headers.get('X-RateLimit-Reset', time.time()))
-            current_time = int(time.time())
-            wait_time = (reset_time - current_time) // 60
-            print('Reset in {} min'.format(wait_time))
-        else:
-            print('Error: {}'.format(response.status_code))
-    except requests.RequestException as e:
-        print('An error occurred: {}'.format(e))
+    '''Returns the user's location'''
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        user_data = response.json()
+        print(user_data.get('location', 'Location not specified'))
+    elif response.status_code == 403:
+        reset_time = int(response.headers.get('X-RateLimit-Reset', 0))
+        reset= (reset_time - time.time()) / 60
+        print('Reset in {} min'.format(reset))
+    elif response.status_code == 404:
+        print('Not found')
+    else:
+        print('Error: {}'.format(response.status_code))
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print('Usage: ./2-user_location.py <api_url>')
-        sys.exit(1)
-
-    api_url = sys.argv[1]
-    get_user_location(api_url)
+        print('Usage: ./2-user_location.py <API_URL>')
+    else: 
+        get_user_location(sys.argv[1])
