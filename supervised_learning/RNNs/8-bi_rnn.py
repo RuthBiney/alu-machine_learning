2d0cc068585a007509134952b6486f4b831beec8
RuthBiney <r.biney@alustudent.com>
2024-09-14 11:20:36 +0200

    error fixed

--- supervised_learning/RNNs/8-bi_rnn.py
+++ supervised_learning/RNNs/8-bi_rnn.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python3
-'''
-    Script that defines a function def bi_rnn(bi_cell, X, h_0, h_t):
-    that performs forward propagation for a bidirectional RNN:
-'''
-
-
 import numpy as np
-
 
 def bi_rnn(bi_cell, X, h_0, h_T):
     '''
@@ -14,29 +6,34 @@ def bi_rnn(bi_cell, X, h_0, h_T):
 
         parameters:
             bi_cell: an instance of BidirectionalCell
-            X: data
-            h_0: initial hidden state
-            h_T: terminal hidden state
+            X: data input of shape (t, m, i)
+            h_0: initial hidden state of shape (m, h)
+            h_T: terminal hidden state of shape (m, h)
 
         return:
             H: all hidden states
             Y: all outputs
     '''
 
-    t, m, i = X.shape
-    l, m, h = h_0.shape
+    t, m, i = X.shape  # t = time steps, m = batch size, i = input size
+    h = h_0.shape[1]   # h = hidden state size
+
+    # Initialize H with zeros: (t + 1, 2, m, h) -> t+1 time steps, 2 directions, batch size, hidden size
     H = np.zeros((t + 1, 2, m, h))
-    H[0, 0] = h_0
-    H[0, 1] = h_T
+
+    # Set the initial hidden states for both directions
+    H[0, 0] = h_0  # Forward direction
+    H[-1, 1] = h_T  # Backward direction
+
+    # Forward direction (left to right)
     for step in range(t):
-        h_prev, y = bi_cell.forward(H[step, 0], X[step])
-        H[step + 1, 0] = h_prev
-        h_next, y = bi_cell.forward(H[step, 1], y)
-        H[step + 1, 1] = h_next
-        if step == 0:
-            Y = y
-        else:
-            Y = np.concatenate((Y, y))
-    output_shape = Y.shape[-1]
-    Y = Y.reshape(t, 2, m, output_shape)
-    return (H, Y)
+        H[step + 1, 0] = bi_cell.forward(H[step, 0], X[step])
+
+    # Backward direction (right to left)
+    for step in reversed(range(t)):
+        H[step, 1] = bi_cell.backward(H[step + 1, 1], X[step])
+
+    # Outputs for all time steps
+    Y = bi_cell.output(H[1:, 0], H[:-1, 1])
+
+    return H, Y
