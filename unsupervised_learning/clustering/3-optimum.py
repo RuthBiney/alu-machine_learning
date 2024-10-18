cd1eebb1b92b1d6737d23f5310818c779cfdecf9
RuthBiney <r.biney@alutstudent.com>
2024-10-18 15:52:45 +0200

    error

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+"""
+Defines function that tests for the optimum number of clusters by variance
+"""
+
+
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
+
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     if kmax is None or kmax <= kmin:
         return None, None
