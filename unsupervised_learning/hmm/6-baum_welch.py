af88a9f5019f6758cb5b20a8e78e0aa89ac62f38
RuthBiney <r.biney@alutstudent.com>
2024-10-25 14:59:57 +0200

    error

--- unsupervised_learning/hmm/6-baum_welch.py
+++ unsupervised_learning/hmm/6-baum_welch.py
@@ -5,37 +5,35 @@ import numpy as np
 
 
 def forward(Observation, Emission, Transition, Initial):
-    """ Forward function based on task3 """
-    N = Transition.shape[0]  # Hidden States
-    T = Observation.shape[0]  # Observations
+    """ Forward algorithm """
+    N = Transition.shape[0]  # Number of hidden states
+    T = Observation.shape[0]  # Number of observations
     F = np.zeros((N, T))
     F[:, 0] = Initial.T * Emission[:, Observation[0]]
 
     for t in range(1, T):
         for n in range(N):
-            F[n, t] = np.sum(Transition[:, n] * F[:, t - 1]) * \
+            F[n, t] = np.sum(F[:, t - 1] * Transition[:, n]) * \
                 Emission[n, Observation[t]]
     return F
 
 
 def backward(Observation, Emission, Transition, Initial):
-    """ Backward function based on task5 """
+    """ Backward algorithm """
     T = Observation.shape[0]
     N, _ = Emission.shape
     beta = np.zeros((N, T))
-    beta[:, T - 1] = np.ones(N)
+    beta[:, T - 1] = 1
 
     for t in range(T - 2, -1, -1):
         for n in range(N):
-            beta[n, t] = np.sum(Transition[n, :] * beta[:, t + 1]
-                                * Emission[:, Observation[t + 1]])
+            beta[n, t] = np.sum(
+                Transition[n, :] * Emission[:, Observation[t + 1]] * beta[:, t + 1])
     return beta
 
 
-def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
-    """
-    Baum-Welch algorithm for a hidden Markov model
-    """
+def baum_welch(Observations, Transition, Emission, Initial, iterations=1000, tol=1e-6):
+    """ Baum-Welch algorithm for a hidden Markov model """
     N, M = Emission.shape
     T = Observations.shape[0]
 
@@ -45,23 +43,26 @@ def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
 
         xi = np.zeros((N, N, T - 1))
         for t in range(T - 1):
-            denominator = np.dot(np.dot(
-                alpha[:, t].T, Transition) * Emission[:, Observations[t + 1]].T, beta[:, t + 1]) + 1e-8
+            denominator = np.dot(
+                alpha[:, t].T, Transition) @ (Emission[:, Observations[t + 1]] * beta[:, t + 1]) + 1e-8
             for i in range(N):
                 numerator = alpha[i, t] * Transition[i, :] * \
-                    Emission[:, Observations[t + 1]].T * beta[:, t + 1].T
+                    Emission[:, Observations[t + 1]] * beta[:, t + 1]
                 xi[i, :, t] = numerator / denominator
 
         gamma = np.sum(xi, axis=1)
-        Transition = np.sum(xi, 2) / np.sum(gamma,
-                                            axis=1).reshape((-1, 1)) + 1e-8
+        Transition_update = np.sum(xi, axis=2) / \
+            (np.sum(gamma, axis=1)[:, None] + 1e-8)
 
         gamma = np.hstack(
-            (gamma, np.sum(xi[:, :, T - 2], axis=0).reshape((-1, 1))))
-
+            (gamma, np.sum(xi[:, :, T - 2], axis=0).reshape(-1, 1)))
         denominator = np.sum(gamma, axis=1) + 1e-8
         for s in range(M):
-            Emission[:, s] = np.sum(gamma[:, Observations == s], axis=1)
-        Emission = np.divide(Emission, denominator.reshape((-1, 1)) + 1e-8)
+            Emission[:, s] = np.sum(
+                gamma[:, Observations == s], axis=1) / denominator
+
+        if np.allclose(Transition, Transition_update, atol=tol) and np.allclose(Emission, Emission, atol=tol):
+            break
+        Transition = Transition_update
 
     return Transition, Emission
