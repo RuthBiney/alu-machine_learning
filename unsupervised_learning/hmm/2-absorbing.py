405fbc0cad449885d077899213ee5c23d708fef7
RuthBiney <r.biney@alutstudent.com>
2024-10-25 14:34:28 +0200

    Updated

--- unsupervised_learning/hmm/2-absorbing.py
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python3
-"""
-Defines function that determines if the Markov Chain is absorbing
-"""
+'''
+    function def absorbing(P): that
+    determines if a markov chain is absorbing
+'''
 
 
 import numpy as np
 
 
 def absorbing(P):
-    """
-    Determines if the Markov Chain is absorbing
-
-    parameters:
-        P [square 2D numpy.ndarray of shape (n, n)]:
-            representing the standard transition matrix
-            P[i, j] is the probability of transitioning from state i to state j
-            n: the number of state in the Markov Chain
-
-    returns:
-        True, if absorbing
-        False, if not absorbing or on failure
-    """
-    # check that P is the correct type and dimensions
-    if type(P) is not np.ndarray or len(P.shape) != 2:
+    '''
+        Determines if a markov chain is absorbing
+    '''
+    if len(P.shape) != 2:
+        return None
+    n1, n2 = P.shape
+    if (n1 != n2) or type(P) is not np.ndarray:
+        return None
+    D = np.diagonal(P)
+    if (D == 1).all():
+        return True
+    if not (D == 1).any():
         return False
-    # save value of n and check that P is square
-    n, n_check = P.shape
-    if n != n_check:
-        return False
+
+    for i in range(n1):
+        # print('this is Pi {}'.format(P[i]))
+        for j in range(n2):
+            # print('this is Pj {}'.format(P[j]))
+            if (i == j) and (i + 1 < len(P)):
+                if P[i + 1][j] == 0 and P[i][j + 1] == 0:
+                    return False
     return True
