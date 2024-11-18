4e1fa925a5eb8f7a2863c0412de1aa9b196335c0
RuthBiney <r.biney@alutstudent.com>
2024-11-18 23:33:32 +0200

    Update

--- reinforcement_learning/deep_q_learning/play.py
+++ reinforcement_learning/deep_q_learning/play.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-"""
-Script that displays a game played by the agent trained in `train.py`
-to play Atari's Breakout
-"""
-
-
 import gym
 import tensorflow.keras as K
 from rl.agents.dqn import DQNAgent
